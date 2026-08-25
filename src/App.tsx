6937fe68eda2b427bd3f1ab5efd851c7af43831e
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-25 17:24:23 -0300

    feat: add styling and components for pet scheduling app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import "./App.css";
 
+import iconPet from "../src/assets/pet.svg";
+
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div>
+      <button className="button-pet">
+        <img src={iconPet} alt="ícone de pet" />
+        <span>MUNDO PET</span>
+      </button>
+
+      <div className="container">
+        <div className="agenda-info">
+          <h1>Sua agenda</h1>
+          <p>
+            Aqui você pode ver todos os clientes e serviços agendados para hoje.
+          </p>
+        </div>
+
+        <div>
+          <input type="date" className="date-selector" />
+        </div>
+      </div>
+    </div>
   );
 }
 
