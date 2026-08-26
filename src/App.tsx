7a22a1f8372fa4230ba007f34ba4656e568feb93
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-25 21:29:26 -0300

    feat: implement Header component and associated styles for pet scheduling app

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
-import "./App.css";
+import { Header } from "./components/Header";
 
-import iconPet from "../src/assets/pet.svg";
-
-function App() {
+export default function App() {
   return (
     <div>
-      <button className="button-pet">
-        <img src={iconPet} alt="ícone de pet" />
-        <span>MUNDO PET</span>
-      </button>
-
-      <div className="container">
-        <div className="agenda-info">
-          <h1>Sua agenda</h1>
-          <p>
-            Aqui você pode ver todos os clientes e serviços agendados para hoje.
-          </p>
-        </div>
-
-        <div>
-          <input type="date" className="date-selector" />
-        </div>
-      </div>
+      <Header />;
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import "../components/headerModules.css";
+import iconPet from "../assets/petIcon.svg";
+
+export function Header() {
+  return (
+    <div>
+      <button className="button-pet">
+        <img src={iconPet} alt="ícone de pet" />
+        <span>MUNDO PET</span>
+      </button>
+
+      <div className="container">
+        <div className="agenda-info">
+          <h1>Sua agenda</h1>
+          <p>
+            Aqui você pode ver todos os clientes e serviços agendados para hoje.
+          </p>
+        </div>
+
+        <div>
+          <input type="date" className="date-selector" />
+        </div>
+      </div>
+    </div>
+  );
+}
