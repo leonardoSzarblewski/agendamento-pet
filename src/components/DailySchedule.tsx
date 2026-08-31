8664c0d8ff990cc08514993374e64d91837b3987
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-31 15:58:51 -0300

    feat: implement daily schedule component with localStorage integration

--- src/components/DailySchedule.tsx
+++ src/components/DailySchedule.tsx
@@ -1,4 +1,6 @@
+import { AxiosError } from "axios";
 import "./dailySchedule.modules.css";
+import { useEffect, useState } from "react";
 
 type Props = {
   icon: string;
@@ -6,7 +8,40 @@ type Props = {
   time: string | number;
 };
 
+type Scheduling = {
+  time: string;
+  owner: string;
+  petName: string;
+  description: string;
+};
+
 export function DailySchedule({ icon, period, time }: Props) {
+  const [data, setDataForm] = useState<Scheduling | null>(null);
+
+  useEffect(() => {
+    try {
+      const dataSave = JSON.parse(
+        localStorage.getItem("agendamento") ?? "null",
+      );
+      setDataForm(dataSave);
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message);
+      }
+
+      setDataForm(null);
+    }
+  }, []);
+
+  function remove() {
+    localStorage.removeItem("agendamento");
+    setDataForm(null);
+
+    alert("Agendamento removido com sucesso!");
+  }
+
   return (
     <div className="container-daily-schedule">
       <div className="header-daily-schedule">
@@ -20,18 +55,24 @@ export function DailySchedule({ icon, period, time }: Props) {
         </div>
       </div>
 
-      <div className="main-daily-schedule">
-        <div className="time">
-          <p>09:00</p>
-          <span>
-            Thor / <strong>Leonardo</strong>
-          </span>
+      {data ? (
+        <div className="main-daily-schedule">
+          <div className="time">
+            <p>{data.time}</p>
+            <span>
+              {data.owner} / <strong>{data.petName}</strong>
+            </span>
+          </div>
+
+          <p className="service">{data.description}</p>
+
+          <button className="button-daily-schedule" onClick={remove}>
+            Remover agendamento
+          </button>
         </div>
-
-        <p className="service">Vacinação</p>
-
-        <button className="button-daily-schedule">Remover agendamento</button>
-      </div>
+      ) : (
+        <p className="message-scheduling">Nenhum agendamento salvo.</p>
+      )}
     </div>
   );
 }
