72ea72209529e25d6a162cf9c561ae32356826ae
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-09-01 14:13:10 -0300

    feat: enhance time selection with clock icon and improve input styles

--- src/page/SchedulingForm.tsx
+++ src/page/SchedulingForm.tsx
@@ -9,6 +9,7 @@ import user from "../assets/user.svg";
 import petIcon from "../assets/petIcon.svg";
 import phone from "../assets/phone.svg";
 import calendar from "../assets/calendar.svg";
+import clock from "../assets/clock.svg";
 
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
@@ -194,27 +195,31 @@ export function SchedulingForm() {
                   render={({ field }) => (
                     <div className="form-group">
                       <label>Hora</label>
-                      <select
-                        className={`input-with-icon ${errors.time ? "input-error" : ""}`}
-                        {...field}
-                      >
-                        <option value="" disabled>
-                          Selecione
-                        </option>
-                        <option value="09:00">09:00</option>
-                        <option value="10:00">10:00</option>
-                        <option value="11:00">11:00</option>
-                        <option value="12:00">12:00</option>
-                        <option value="13:00">13:00</option>
-                        <option value="14:00">14:00</option>
-                        <option value="15:00">15:00</option>
-                        <option value="16:00">16:00</option>
-                        <option value="17:00">17:00</option>
-                        <option value="18:00">18:00</option>
-                        <option value="19:00">19:00</option>
-                        <option value="20:00">20:00</option>
-                        <option value="21:00">21:00</option>
-                      </select>
+                      <div className="select-container">
+                        <img src={clock} alt="ícone de relógio" />
+                        <select
+                          className={`input-with-icon ${errors.time ? "input-error" : ""}`}
+                          {...field}
+                        >
+                          <option value="" disabled>
+                            Selecione
+                          </option>
+
+                          <option value="09:00">09:00</option>
+                          <option value="10:00">10:00</option>
+                          <option value="11:00">11:00</option>
+                          <option value="12:00">12:00</option>
+                          <option value="13:00">13:00</option>
+                          <option value="14:00">14:00</option>
+                          <option value="15:00">15:00</option>
+                          <option value="16:00">16:00</option>
+                          <option value="17:00">17:00</option>
+                          <option value="18:00">18:00</option>
+                          <option value="19:00">19:00</option>
+                          <option value="20:00">20:00</option>
+                          <option value="21:00">21:00</option>
+                        </select>
+                      </div>
                     </div>
                   )}
                 />
